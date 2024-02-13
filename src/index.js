e4b51dcf66e766a8a81ce9f2eff560b8551af68c
saurabh-magar <magarsaurabh59@gmail.com>
2024-02-13 21:18:11 +0530

    data base connected successfully

---
+++ src/index.js
@@ -0,0 +1,81 @@
+// 2nd and best approach in which we write code in a other file and import here 
+//require("dotenv").config({path:'./env'})
+
+import dotenv from "dotenv"
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB;
+
+
+//--experimental-json-module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+1st approach where we write code in main file index.js it is not a professional apporach
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+//mongoose.connect("mongodb+srv://saurabhmagar:Saurabh2003@cluster0.moyfull.mongodb.net") 
+// above is not a professional approach because we need to write in try and catch to handle errors
+
+// function connectDB(){}
+// connectDB();
+
+const PORT=8000;
+import express from "express"
+const app=express();
+
+(async()=>{
+    try {
+        await mongoose.connect(`${"mongodb+srv://saurabhmagar:Saurabh2003@cluster0.moyfull.mongodb.net"}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port ${PORT}`);
+        })
+    } catch (error) {
+        console.error("ERROR is",error)
+        throw error
+        
+    }
+
+})()
+*/
